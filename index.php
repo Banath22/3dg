35b0d66b78b85fcba465db276df956a433ad917d
Banath22 <casper_vz@hotmail.com>
2015-10-13 13:32:23 +0200

    hi

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Gebruikersnaam, wachtwoord van de database etc ophalen
+require 'includes/config.php' ;
+// Laad de Smarty bibliotheek in
+require_once 'libs/smarty/Smarty.class.php';
+// Voer instellingen uit en stel template parser in
+require 'includes/bootstrap.php' ;
+// Maak de database connectie
+require_once 'includes/database.php' ;
+
+// Koppel de waarde van de paginatitel aan te smarty tag 'title'
+$templateParser->assign('title', 'Me First And The Gimme Gimmes');
+// Toon de template: output html
+$templateParser->display('head.tpl');
+
+// Haal de nieuws artikelen op
+require 'logic/select_newsarticles.php';
+// Toon de nieuwsberichten. Oude stijl:
+// Bouw dit om naar een template systeem
+include 'views/newsarticles.php' ;
+
